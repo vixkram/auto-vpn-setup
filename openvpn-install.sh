02bb1afcf591d3ce67416958f23dc19451227888
Vikram <154753921+vixkram@users.noreply.github.com>
2024-01-07 22:52:44 +0530

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#
-# https://github.com/Nyr/openvpn-install
-#
-# Copyright (c) 2013 Nyr. Released under the MIT License.
+
 
 
 # Detect Debian users running the script with "sh" instead of bash
